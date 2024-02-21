b1bdcba2ab76ad39fb9fab3c69426c9ef3fcd5a4
jayasurya <jayasuryajayan2021@gmail.com>
2024-02-21 08:52:25 +0530

    movie manager

--- movie_manager/movies/models.py
+++ movie_manager/movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class MovieInfo(models.Model):
+    tittle=models.CharField(max_length=250)
+    year=models.IntegerField(null=True)
+    description=models.TextField()
+
+
+class Director(models.Model):
+  name=models.CharField(max_length=300)
+
+
+ 
+ 
--- movie_manager/movies/views.py
+++ movie_manager/movies/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from . models import MovieInfo
+from . models import MovieForm
+
 
 # Create your views here.
 def create(request):
-    return render(request,'create.html')
+    frm=MovieForm()
+    if request.POST:
+        tittle=request.POST.get('tittle')
+        year=request.POST.get('year')
+        description=request.POST.get('description')
+        movie_obj=MovieInfo(tittle=tittle,year=year,description=description)
+        movie_obj.save()
+        
+    return render(request,'create.html',{'frm':frm})
+
+def list(request):
+    movie_set=MovieInfo.objects.all       
+    print(movie_set) 
+    return render(request,'list.html',{'movies':movie_set})
+
+def edit(request):
+    return render(request,'edit.html')
+
+
+
